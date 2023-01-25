6542764aba7b5f8347ed1c87c8f1f41d9b64887b
Alexandr Yakovlev <engelsovich75@yandex.ru>
2023-01-25 16:51:41 +0500

    Task 2

--- src/Basket.java
+++ src/Basket.java
@@ -1,18 +1,14 @@
 import java.io.*;
 
-public class Basket {
-    private String[] products; // массив продуктов
-    private int[] prices; // массив цен
-    private int[] arrCountProducts; // массив количества товаров
+public class Basket implements Serializable {
+    private final String[] products; // массив продуктов
+    private final int[] prices; // массив цен
+    private final int[] arrCountProducts; // массив количества товаров
 
     public Basket(String[] products, int[] prices) {
         this.products = products;
         this.prices = prices;
         this.arrCountProducts = new int[products.length];
-    }
-
-    private Basket() {
-
     }
 
     // метод добавления amount штук продукта номер productNum в корзину
@@ -34,47 +30,25 @@ public class Basket {
         System.out.println("Итого " + sumProducts + " руб");
     }
 
-    // метод сохранения корзины в текстовый файл
-    public void saveTxt(File textFile) throws IOException {
-        try (FileWriter writer = new FileWriter(textFile)) {
-            writer.write(products.length + "\n"); // записываем в первой строке размер массива продуктов
-            for (int i = 0; i < products.length; i++) {
-                writer.write(products[i] + "\t" + prices[i] + "\t" + arrCountProducts[i] + "\n");
-            }
+    // метод сохранения в файл в бинарном формате
+    public void saveBin (File file) {
+        try (FileOutputStream fos = new FileOutputStream(file);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(this);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 
-    // метод восстановления объекта корзины из текстового файла, в который ранее была она сохранена
-    public static Basket loadFromTxtFile(File textFile) {
-        String[] products = null;
-        int[] prices = null;
-        int[] arrCountProducts = null;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(textFile))) {
-            int size = Integer.parseInt(reader.readLine()); // считываем число для размера создаваемых массивов
-
-            products = new String[size];
-            prices = new int[size];
-            arrCountProducts = new int[size];
-
-            String s;
-
-            for (int i = 0; i < size; i++) {
-                s = reader.readLine();
-                String[] parts = s.split("\t");
-                products[i] = parts[0];
-                prices[i] = Integer.parseInt(parts[1]);
-                arrCountProducts[i] = Integer.parseInt(parts[2]);
-            }
-        } catch (IOException ex) {
+    // метод загрузки корзины из бинарного файла
+    public static Basket loadFromBinFile(File file) {
+        Basket basket = null;
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            basket = (Basket) ois.readObject();
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-
-        Basket basket = new Basket();
-        basket.products = products;
-        basket.prices = prices;
-        basket.arrCountProducts = arrCountProducts;
-
         return basket;
     }
 
